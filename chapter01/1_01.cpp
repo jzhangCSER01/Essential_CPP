b6e68e1f56fbdcc2251fa39a7029b82863868003
jzhangCSER01 <1760616780@qq.com>
2022-06-08 17:19:15 +0800

    第一章部分内容

---
+++ chapter01/1_01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string user_name;
+    cin >> user_name;
+    cout << "Ãû×Ö£º" << user_name << endl; 
+    cout << "hello world" << endl;
+    system("pause");
+    return 0;
+}
---
+++ chapter01/1_02.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string user_name;
+    int user_val;
+    int num_tries, num_right = 0;
+    double user_score = 0.0;
+    char user_more;
+    bool go_for_it = true;
+    const int max_tries = 3;
+    
+    system("pause");
+    return 0;
+}
